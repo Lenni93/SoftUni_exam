6bc0fbbdab5d335301cc2df839bbecec87ffeefd
Lilyana Ivanova <lennibossta@gmail.com>
2024-02-17 17:50:03 +0200

    Update 01.Black_Flag.py

--- 06. Programming Fundamentals Mid Exam Retake/01.Black_Flag.py
+++ 06. Programming Fundamentals Mid Exam Retake/01.Black_Flag.py
@@ -1,18 +1,19 @@
-day = int(input())
+days = int(input())
 daily_plunder = int(input())
 expected_plunder = float(input())
-current_plunder = 0
+
 total_plunder = 0
-for plunder in range(1, day + 1):
+for plunder in range(1, days + 1):
     total_plunder += daily_plunder
 
     if plunder % 3 == 0:
-        total_plunder += 0.50 * daily_plunder
-    elif plunder % 5 == 0:
+        total_plunder += 0.5 * daily_plunder
+
+    if plunder % 5 == 0:
         total_plunder *= 0.7
 
 if total_plunder >= expected_plunder:
     print(f"Ahoy! {total_plunder:.2f} plunder gained.")
-elif total_plunder < expected_plunder:
+else:
     percentage = (total_plunder / expected_plunder) * 100
     print(f"Collected only {percentage:.2f}% of the plunder.")
